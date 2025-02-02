abb233b6f555aaec859f26bdfbb2814e89db71a4
Carlos Eduardo da Silva Oliveira <48035815+KDZsilva@users.noreply.github.com>
2025-02-02 19:12:46 -0300

    adicionei as tabelas com as cidades e estados do Brasil

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call(EstadoSeeder::class);
+        $this->call(CidadeSeeder::class);
     }
 }
