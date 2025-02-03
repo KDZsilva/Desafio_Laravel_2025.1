f4f6f7f35f2ff409e427177d9d9ce5d31c2f380a
Carlos Eduardo da Silva Oliveira <48035815+KDZsilva@users.noreply.github.com>
2025-02-02 22:45:54 -0300

    criei a tabela sales

---
+++ database/migrations/2025_02_02_223608_create_sales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('unidades')->default(1)->unsigned();
+            $table->integer('valor')->unsigned();
+            $table->foreignId('address_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
